6822a942f6c978555287c32672fb0f0df5c717dd
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2026-06-11 10:59:08 +0200

    Initial Commit

---
+++ maps widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  maps widget
+//
+//  Created by Aiday Marlen Kyzy on 11.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ maps widget/maps_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  maps_widgetApp.swift
+//  maps widget
+//
+//  Created by Aiday Marlen Kyzy on 11.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct maps_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
